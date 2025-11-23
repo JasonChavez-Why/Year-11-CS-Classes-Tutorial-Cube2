e16bd29e794645eb841af733d3681a19cf54b877
JasonChavez-Why <jasonc2028@student.cis.edu.hk>
2025-11-23 16:47:18 +0800

    .

--- src/Cube2.java
+++ src/Cube2.java
@@ -12,18 +12,58 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        this();
+        basicCube.setSide(side);
+        this.color = color;
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
-
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
-
+    public void setSide(int side) {
+        basicCube.setSide(side);
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int calculateVolume() {
+        return basicCube.calculateVolume();
+    }
+    public int calculateSurfaceArea() {
+        return basicCube.calculateSurfaceArea();
+    }
+    public boolean equals(Cube2 otherCube) {
+        return this.getSide() == otherCube.getSide() && this.color.equals(otherCube.getColor());
+    }
+    public Cube2 add(Cube2 otherCube) {
+        int thisSide = this.getSide();
+        int otherSide = otherCube.getSide();
+        int newSide = (int) Math.sqrt(thisSide * thisSide + otherSide * otherSide);
+        if (Math.pow(thisSide, 2) + Math.pow(otherSide, 2) == Math.pow(newSide, 2)) {
+            Cube2 result = new Cube2(newSide, this.color);
+            return result;
+        } else {
+            throw new IllegalArgumentException("You idiot");
+        }
+    }
+    public Cube2 minus(Cube2 otherCube) {
+        int thisSide = this.getSide();
+        int otherSide = otherCube.getSide();
+        int largerSide = Math.max(thisSide, otherSide);
+        int smallerSide = Math.min(thisSide, otherSide);
+        int newSideSquared = largerSide * largerSide - smallerSide * smallerSide;
+        int newSide = (int) Math.sqrt(newSideSquared);
+        if (newSide * newSide == newSideSquared && newSide > 0) {
+            return new Cube2(newSide, this.color);
+        } else {
+            throw new IllegalArgumentException("You idiot");
+        }
+    }
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + getSide() + ", color=\"" + color + "\"}";
     }
 }
